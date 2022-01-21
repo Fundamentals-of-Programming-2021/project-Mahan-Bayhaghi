af70c831909591868afb9070bce5ceea44512fe1
Mahan-Bayhaghi <mahan.bayhaghi@gmail.com>
2022-01-21 21:51:17 +0330

    add soldier number and modified map display

--- src/main.c
+++ src/main.c
@@ -7,13 +7,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 #include <time.h>
 
 const int WIDTH = 800 ;
 const int HEIGHT = 600 ;
-const int FPS = 60 ;
+const int FPS = 80 ;
 const int PRO_RATE = 3 ;
 const int INIT_SOLDIER = 25 ;
 const int NUMBER_OF_PLAYERS = 3 ;
@@ -24,7 +22,7 @@ typedef  struct properties {
     int state_condition  ;          // -1 --> null // 0 --> neutral // 1 --> in_use //
     int owner ;                     // a number between 0 to (num_of_players) // 0 for neutral // other nums for players //
     int soldiers ;                  // always between 0 and 120 //
-    int soldier_production_rate ;   // 0 for non-padegans and PRO_RATE for others //
+    int soldier_production_rate ;   // 0 for non-military and PRO_RATE for others //
 } properties ;
 
 void create_map_array ( const char* map_name , properties arr[10][20])
@@ -64,7 +62,7 @@ void create_map_array ( const char* map_name , properties arr[10][20])
     for ( int i=0 ; i<10 ; i++)
     {
         for ( int j=0 ; j<20 ; j++)
-            printf("%d " , arr[i][j].soldiers ) ;
+            printf("%4d" , arr[i][j].soldiers ) ;
         printf("\n") ;
     }
 }
@@ -74,7 +72,7 @@ void RANDOMIZE_MAP ( properties arr[10][20] , int players_num )
     // 0 --> NEUTRAL LAND WITH NO MILITARY BASE
     // 1 TO players_num --> PLAYERS' LAND WITH MILITARY BASE AT THE BEGINNING OF THE GAME
     // ALL PLAYERS WILL HAVE AT LEAST 1 AND AT MAX 3 LANDS AT THE BEGINNING OF THE GAME
-    srand(time(NULL)) ;
+    srand(time(0)) ;
     int land_num ;
     int rand_x ;
     int rand_y ;
@@ -98,18 +96,21 @@ void RANDOMIZE_MAP ( properties arr[10][20] , int players_num )
 
 void DRAW_LAND ( SDL_Renderer *renderer , properties land , int length , int height , Sint16 x , Sint16 y )
 {
-    SDL_Rect border = { .x=x-length/2 , .y=y-height/2 , .w = length , .h=height } ;
-
-    if ( land.state_condition == -1 )
-        boxColor(renderer , x-length/2 , y-height/2 , x+length/2 , y+height/2 , 0xffffff00 ) ;
-    else
+//    if ( land.state_condition == -1 )
+//        boxColor(renderer , x-length/2 , y-height/2 , x+length/2 , y+height/2 , 0xffffffff ) ;
+    if ( land.state_condition != -1 )
     {
-        boxColor(renderer, x - length / 2, y - height / 2, x + length / 2, y + height / 2, COLORS[land.owner]);
-        if ( land.soldier_production_rate != 0 )
-            filledCircleColor(renderer , x-length/4 , y-height/4 ,  2 , 0xff000000) ;
+        SDL_Rect border = { .x=x-length/2 , .y=y-height/2 , .w = length , .h=height } ;
+        boxColor(renderer, x-length/2, y-height/2, x+length/2, y+height/2, COLORS[land.owner]);
+        if ( land.soldier_production_rate != 0 ) {
+            filledCircleColor(renderer, (x), (y), 2, 0xff000000);
+            char* buffer = malloc(sizeof (char) * 20) ;
+            sprintf(buffer , "%d" , land.soldiers) ;
+            stringRGBA(renderer , x-5 , y+5 , buffer , 0 , 0 , 0 , 255) ;
+        }
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
+        SDL_RenderDrawRect(renderer, &border);
     }
-    SDL_SetRenderDrawColor(renderer , 0x00 , 0x00 , 0x00 , 0xff) ;
-    SDL_RenderDrawRect(renderer , &border ) ;
 }
 
 void GAME_BACKGROUND (SDL_Renderer *renderer , properties arr[10][20] , int length , int height )
@@ -122,9 +123,9 @@ void GAME_BACKGROUND (SDL_Renderer *renderer , properties arr[10][20] , int leng
         for ( int j=0 ; j<20 ; j++)
         {
             DRAW_LAND(renderer , arr[i][j] , length , height , x , y ) ;
-            x += length/2 ;
+            x += length ;
         }
-        y += length/2 ;
+        y += height ;
     }
 }
 
@@ -135,7 +136,7 @@ int main()
         return 0;
     }
 
-    SDL_Window *sdlWindow = SDL_CreateWindow("Stateio" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , WIDTH , HEIGHT , SDL_WINDOW_OPENGL) ;
+    SDL_Window *sdlWindow = SDL_CreateWindow("State.io" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , WIDTH , HEIGHT , SDL_WINDOW_OPENGL) ;
     SDL_Renderer *sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED) ;
 
     properties map_array[10][20] ;
@@ -150,14 +151,18 @@ int main()
     int little_circle = 0 ;
     while ( !shallExit )
     {
+        i++ ;
         SDL_SetRenderDrawColor(sdlRenderer , 0xff , 0xff , 0xff ,0xff) ;
         SDL_RenderClear(sdlRenderer) ;
 
-        GAME_BACKGROUND(sdlRenderer , map_array , 65 , 60) ;
+        GAME_BACKGROUND(sdlRenderer , map_array , 65 , 45) ;
+
+//        char* buffer = malloc(sizeof (char) * 50) ;
+//        sprintf(buffer , "my score is %d " , i/75) ;
+//        stringRGBA(sdlRenderer , 5 , 5 , buffer , 0 , 0 , 0 , 255) ;
 
 
         SDL_Event sdlEvent;
-
         while (SDL_PollEvent(&sdlEvent)) {
             int x = sdlEvent.motion.x ;
             int y = sdlEvent.motion.y ;
@@ -175,6 +180,7 @@ int main()
                 case SDL_MOUSEBUTTONDOWN:
                     little_circle = 1 ;
                     printf("mouse is being pushed at (%d , %d)\n" , x , y) ;
+                    printf("dar khane (%d,%d) array hastim " , x/65 , y/45 ) ;
                     break;
                 case SDL_MOUSEBUTTONUP:
                     printf("mouse is up now\n") ;
@@ -187,6 +193,8 @@ int main()
         i++;
     }
 
+
+    // Quitting window and program //
     SDL_DestroyWindow(sdlWindow) ;
     SDL_Quit() ;
     printf("all done") ;
