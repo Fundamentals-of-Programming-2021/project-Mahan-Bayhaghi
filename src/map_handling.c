a3e4a495083a6e78883318aaa376a73febf0c0e3
Mahan-Bayhaghi <mahan.bayhaghi@gmail.com>
2022-01-31 21:16:01 +0330

    add menu and some header files

---
+++ src/map_handling.c
@@ -0,0 +1,5 @@
+//
+// Created by Mahan on 1/31/2022.
+//
+
+#include "map_handling.h"
---
+++ src/map_handling.h
@@ -0,0 +1,8 @@
+//
+// Created by Mahan on 1/31/2022.
+//
+
+#ifndef MAIN_C_MAP_HANDLING_H
+#define MAIN_C_MAP_HANDLING_H
+
+#endif //MAIN_C_MAP_HANDLING_H
